a8c79116b21c063a45e73d978bdd7fe826b61011
Yuki <uni.1010@me.com>
2020-08-03 12:42:59 +0000

    php03kadai

---
+++ 03kadai.php
@@ -0,0 +1,30 @@
+<?php
+//課題1
+$name = "yuki";
+if($name == "yuki"){
+     $name.="です"; 
+      echo "私は$name" . "\n";
+} else {
+     echo "yukiではありません" . "\n";
+}
+//課題2
+$total = 0;
+for($i = 1;$i <= 10000;$i++){ 
+    $total += $i;}
+ echo $total . "\n";
+ //課題3
+$fruits = array("apple","banana","cherry","strawberry ","peach ");
+foreach($fruits as $fruit){
+    echo $fruit . "\n";
+}
+//課題4
+$start = 1;
+$end = 100;
+for($i = $start; $i <= $end; $i++){
+    if($i % 5 == 0){
+        echo $i;
+    }
+}
+
+?>
+ 
